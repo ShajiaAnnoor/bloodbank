7b05269488b5f415cce751c43b8fa3634e8acaa8
ShajiaAnnoor <shajia.annoor@gmail.com>
2021-05-30 04:16:59 +0600

    Add component for redux store

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { StyleSheet } from 'react-native';
+import MyApp from './MyApp/myApp';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <MyApp />
+     </PersistGate>
+    </Provider>
   );
 }
 
