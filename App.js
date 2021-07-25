9dc44f728a832d1a331f2ab9585ff8ef8c4a9858
ShajiaAnnoor <shajia.annoor@gmail.com>
2021-07-26 01:46:45 +0600

    Import tab navigations

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from "react-redux";
 import { StyleSheet } from 'react-native';
-import MyApp from './MyApp/myApp';
+import MyTab from './MyApp/MyTab';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from "./redux/store";
 
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <MyApp />
+        <MyTab />
      </PersistGate>
     </Provider>
   );
