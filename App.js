1377e4f6df39ab40a61950f5fbf7183a9d827cbe
ShajiaAnnoor <shajia.annoor@gmail.com>
2021-07-28 21:01:48 +0600

    Import a component in root component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from "react-redux";
 import { StyleSheet } from 'react-native';
 import MyTab from './MyApp/MyTab';
+import My from './MyApp/myApp';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from "./redux/store";
 
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <MyTab />
+        <My/>
      </PersistGate>
     </Provider>
   );
