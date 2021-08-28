303222fe990a2c1d5a373b6cd46c1d1b6640a96f
ShajiaAnnoor <shajia.annoor@gmail.com>
2021-08-29 02:42:00 +0600

    Add profile pages

---
+++ Profile/Organization/OrganizationModal/districtModal.js
@@ -0,0 +1,122 @@
+import React, { 
+    useState, 
+} from "react";
+import { 
+    Alert, 
+    Modal,
+    Pressable, 
+    StyleSheet, 
+    Text, 
+    View, 
+    TextInput,
+    TouchableOpacity
+} from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+const districtEntryModal = ({mv, toggle, setDistrict, index}) => {
+  const [yourDistrict, setYourDistrict] = useState(' ');
+  
+  const handleModalSubmit = () => {
+    toggle(index);
+    setDistrict(yourDistrict);
+  }
+
+  return (
+    <Modal
+      statusBarTranslucent={true}
+      animationType="fade"
+      transparent={true}
+      visible={mv}
+      onRequestClose={() => {
+        Alert.alert("Modal has been closed.");
+        }}
+    >
+
+      <View style={styles.modalView}>
+        <Text style={styles.logo}>
+          Change/Edit your district
+        </Text>
+
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Your District..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => setYourDistrict(text)}
+          />
+        </View>
+          
+        <View>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={handleModalSubmit}
+          >
+            <Text style={styles.textStyle}>POST</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+   
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius:10,
+    padding: 30,
+    elevation: 2,
+    width: '90%',
+    alignSelf:'center',
+     margin:10,
+    justifyContent:'space-around',
+  },
+  buttonOpen: {
+    backgroundColor: "#c71010",
+  },
+  buttonClose: {
+    backgroundColor: "#c71010",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  logo:{
+    fontWeight:"bold",
+    fontSize:30,
+    color:"#590208",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#ebd8d9",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+ 
+});
+
+export default districtEntryModal;
---
+++ Profile/Organization/OrganizationProfile/OrganizationProfile.js
@@ -0,0 +1,128 @@
+import React, { useState, Fragment } from "react";
+import { View,Button, Switch, StyleSheet,Text,TextInput, TouchableOpacity } from "react-native";
+import { Paragraph } from 'react-native-paper';
+import { Octicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import NameEntryModal from './NameModal2';
+import AddressEntryModal from './AddressModal';
+import DistrictEntryModal from './DistrictModal';
+import ContactEntryModal from './ContactModal';
+
+const OrganizationProfile = () => {
+  const arr = [false,false,false,false];
+  const[myName, setMyName] = useState("");
+  const[myAddress, setMyAddress] = useState("");
+  const[myDistrict, setMyDistrict] = useState("");
+  const[myContact, setMyContact] = useState("");
+  const[status, setStatus] = useState("Ready");
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [isReadyEnabled, setIsReadyEnabled] = useState(false);
+  const[ modalVisible, setModalVisible] = useState(arr);
+  const[tmpToggle,setTmpToggle] = useState(false);
+
+  const toggleSwitch = () => setIsEnabled(previousState => {
+    return !previousState; 
+  })
+  const toggleReadySwitch = () => setIsReadyEnabled(previousState => !previousState);
+  const toggleModalVisiblity = (i) => setModalVisible(previousState => {
+    let newArr = [];
+
+    //alert("Prev" + previousState);
+    for (var j of previousState) {
+      newArr.push(j);
+    }
+
+    if( newArr[ i ] == false ) {
+      newArr[ i ] = true ; 
+    } else {
+      newArr[ i ] = false ; 
+    }
+    //newArr = previousState ;
+    //newArr[i] = !previousState[i];
+    //alert("new arr fu"+newArr);
+    //alert("index is" + i);
+    return newArr ;
+  })
+
+  const toggleEverything = () => {
+    toggleReadySwitch();
+    if( status === "Ready" ) {
+      setStatus("Not Ready");
+    } else {
+      setStatus("Ready");
+    }
+  }
+
+  let text;
+    
+  if (isEnabled) {      
+    text = <Text>Not Available</Text>;    
+  } else {      
+    text = <Text>Available</Text>;    
+  }
+ // alert(modalVisible);
+  return (
+    <Fragment>
+       <NameEntryModal mv={modalVisible[0]} toggle={toggleModalVisiblity} setName={setMyName} index={0}/>
+       <AddressEntryModal mv={modalVisible[1]} toggle={toggleModalVisiblity} setAddress={setMyAddress} index={1}/>
+        <DistrictEntryModal mv={modalVisible[2]} toggle={toggleModalVisiblity} setDistrict={setMyDistrict} index={2}/>
+         <ContactEntryModal mv={modalVisible[3]} toggle={toggleModalVisiblity} setContact={setMyContact} index={3}/>
+    <View style={styles.container1}>
+      <View style={styles.itemContainer2}>
+        <Ionicons name="person-circle" size={24} color="black" />
+        <Text>{myName}</Text>
+        <Button title="edit me" onPress={()=>toggleModalVisiblity(0)}/>
+      </View>
+
+      <View style={styles.itemContainer2}>
+        <Octicons name="location" size={24} color="black" />
+        <Text>{myAddress}</Text>
+        <Button title="Edit" onPress={()=>toggleModalVisiblity(1)}/>
+      </View>
+
+      <View style={styles.itemContainer2}>
+        <Octicons name="location" size={24} color="black" />
+        <Text>{myDistrict}</Text>
+        <Button title="Edit" onPress={()=>toggleModalVisiblity(2)}/>
+      </View>
+
+      <View style={styles.itemContainer2}>
+       
+        <FontAwesome name="phone-square" size={24} color="black" />
+        <Text>{myContact}</Text>
+        <Button title="Edit" onPress={()=>toggleModalVisiblity(3)}/>
+        
+      </View>   
+        
+    </View>
+  </Fragment>
+  );
+}
+
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",  
+  },
+
+  itemContainer2: {
+    //flex:1,
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    height:"10%",
+    width:"80%",
+    backgroundColor:"#ebd8d9",
+    margin:10,
+    //marginBottom:3,
+    //marginTop:10,
+    alignItems:"center",
+    //borderRadius:25,  
+  },
+});
+
+export default OrganizationProfile;
