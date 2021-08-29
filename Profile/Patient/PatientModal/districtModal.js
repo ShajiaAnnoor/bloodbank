d4a6ab8a05cd0450eeb55d3f810f21132fd1ceed
Shajia Annoor <shajiaannoor@gmail.com>
2021-08-30 02:46:14 +0600

    Remove redundant import and remove redundant comments

--- Profile/Patient/PatientModal/districtModal.js
+++ Profile/Patient/PatientModal/districtModal.js
@@ -9,9 +9,7 @@ import {
     Text, 
     View, 
     TextInput,
-    TouchableOpacity
 } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
 
 const districtEntryModal = ({mv, toggle, setDistrict, index}) => {
   const [yourDistrict, setYourDistrict] = useState(' ');
