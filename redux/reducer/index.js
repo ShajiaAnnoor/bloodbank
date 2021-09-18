28b758047ea4abc4818228ebff50998e92362838
Shajia Annoor <shajiaannoor@gmail.com>
2021-09-19 01:16:52 +0600

    Update root reducers with new branches

--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -2,13 +2,16 @@ import { combineReducers } from "redux";
 import subscriptionList from "./subscriptionlist";
 import donorList from "./donorList";
 import patientList from  "./patientList";
+import organizationList from "./organizationLIst";
+import requestList from "./requestList";
 
 const rootReducer = combineReducers({
     subscriptionList,
     donorList,
     patientList,
+    organizationList,
+    requestList,
 //    "updateDonors": updateDonors,
-
 })
 
 export default (state,action) => rootReducer(state,action);
