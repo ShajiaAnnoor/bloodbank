702d0686c01d6fb984d35648f4d4e72211a9db81
Shajia Annoor <shajiaannoor@gmail.com>
2021-09-19 01:11:16 +0600

    Add reducer for organization list

---
+++ redux/reducer/organizationLIst.js
@@ -0,0 +1,28 @@
+import {
+    SET_ORGANIZATION_LIST,
+    FETCH_ORGANIZATION_LIST,
+  } from "../constants/constants";
+  
+
+
+  const initialState = {
+    organizationList: [],
+    isLoading: false,
+  }
+  
+  export default (state = initialState, action) => {
+    switch(action.type) {
+      case SET_ORGANIZATION_LIST:
+        return {
+          organizationList: action.payload,
+          isLoading: false,
+        };
+    case FETCH_ORGANIZATION_LIST:
+        return{
+            ...state,
+            isLoading: true,
+        }
+        default:
+          return state;
+    }
+  }
