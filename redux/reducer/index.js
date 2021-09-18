5ecf2ab830e84ad85a316f78e510b436c060fc6b
Shajia Annoor <shajiaannoor@gmail.com>
2021-09-19 01:10:44 +0600

    Add reducer of patient list to combine reducers

--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import subscriptionList from "./subscriptionlist";
 import donorList from "./donorList";
+import patientList from  "./patientList";
 
 const rootReducer = combineReducers({
     subscriptionList,
     donorList,
+    patientList,
 //    "updateDonors": updateDonors,
 
 })
