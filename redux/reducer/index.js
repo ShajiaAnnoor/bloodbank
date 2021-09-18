6d05a69fa5a7f6e74b15491128e0d53881549131
Shajia Annoor <shajiaannoor@gmail.com>
2021-09-18 18:33:58 +0600

    Add reducer for donor list

--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import subscriptionList from "./subscriptionlist";
+import donorList from "./donorList";
 
 const rootReducer = combineReducers({
-    "subscriptionList" : subscriptionList,
+    subscriptionList,
+    donorList,
+//    "updateDonors": updateDonors,
+
 })
 
 export default (state,action) => rootReducer(state,action);
