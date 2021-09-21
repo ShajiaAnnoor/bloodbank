32ac09d1ab1af8dd4b5ecec77d773e78f9431c2a
Shajia Annoor <shajiaannoor@gmail.com>
2021-09-21 15:42:03 +0600

    Remove redundant code

--- Forms/requestOrganizationEntryModal.js
+++ Forms/requestOrganizationEntryModal.js
@@ -2,7 +2,6 @@ import React, {
     useState, 
 } from "react";
 import { 
-    Alert, 
     Modal,
     Pressable, 
     StyleSheet, 
@@ -126,26 +125,11 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
- /* button: {
-    borderRadius:10,
-    padding: 30,
-    elevation: 2,
-    width: '90%',
-    alignSelf:'center',
-     margin:10,
-    justifyContent:'space-around',
-  },*/
   button: {
     borderRadius:10,
-    //padding: 30,
-    //elevation: 2,
-   // width: '90%',
-    //alignSelf:'center',
     margin:10,
     justifyContent:'space-around',
-   // margin: 10,
     padding:30,
-    //backgroundColor: '#FFF',
     width: '100%',
     height: '90%',
     flex: 9,
@@ -161,11 +145,7 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     textAlign: "center",
-    //color: "white",
-    //fontWeight: "bold",
-    //textAlign: "center",
     flexWrap:'wrap',
-    //marginBottom:5,
     padding:1,
   },
   modalText: {
@@ -176,8 +156,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#eda187',
     alignItems: 'center',
-    justifyContent: 'center',
-    
+    justifyContent: 'center',    
   },
   container2: {
     margin: 50,
