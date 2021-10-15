ce1608641895577f601b4ba5f224fb6f344d6d8d
Shajia Annoor <shajiaannoor@gmail.com>
2021-10-15 17:20:02 +0600

    Refactor style to another file

--- MyApp/myApp.js
+++ MyApp/myApp.js
@@ -3,13 +3,13 @@ import React, {
 } from 'react';
 import {
   Image,
-  StyleSheet,
   Switch,
   View,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { styles } from "./myApp.style";
 import MyTab from '../MyApp/MyTab';
 import ProfileNavigator from '../Profile/ProfileNavigatorButton/profileNavigatorButton.js'
 //import Patient from "../Profile/Patient/PatientProfile/PatientProfile";
@@ -102,49 +102,3 @@ export default function MyApp() {
   );
 }
 
-const styles = StyleSheet.create({
-  headerRightStyle: {
-    flex:1,
-    flexDirection: 'row',
-    padding:10,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginRight:25,
-    width:'80%',
-  },
-  listItem: {
-    margin: 10,
-    padding: 10,
-    backgroundColor: '#a7d1c9',
-    width: '80%',
-    flex: 1,
-    alignSelf: 'center',
-    flexDirection: 'column',
-    borderRadius: 5,
-    alignItems:'flex-start',
-    justifyContent: 'space-between',
-  },
-  filterContainer: {
-    flex: 1,
-    paddingTop:39,
-    alignItems: "flex-start",
-    flexDirection:'row',
-    justifyContent: 'space-between',
-    margin:2,
-  },
-
-  filterButton:{
-    height:40,
-    width:6,
-    margin:30,
-    padding:30,
-    backgroundColor:"#4CAF50",
-  },
-
-  drawerContainer: {
-    flex: 1,
-    backgroundColor: 'black',
-    padding: 5,
-    borderRadius:50,
-},
-});
