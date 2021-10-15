17c924ec1f76cc60e7d0dfc4a85b5efd69f5da0a
Shajia Annoor <shajiaannoor@gmail.com>
2021-10-15 17:42:32 +0600

    Update import paths

--- MyApp/MyTab.js
+++ MyApp/MyTab.js
@@ -10,10 +10,10 @@ import { SimpleLineIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
-import  Organizations from '../List/organizationList';
-import Donors from '../List/donorList';
-import  Requests from '../List/requestList';
-import Patients from '../List/patientList';
+import  Organizations from '../List/Organization';
+import Donors from '../List/Donor';
+import  Requests from '../List/Request';
+import Patients from '../List/Patient';
 
 /*function Patients() {
   return (
