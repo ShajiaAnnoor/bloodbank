bb3efddc0a682a41890fe6643be3cc295d5cfc6b
Shajia Annoor <shajiaannoor@gmail.com>
2021-10-15 18:43:04 +0600

    Refactor style to another file

--- Forms/organizationEntry.js
+++ Forms/organizationEntry.js
@@ -2,13 +2,14 @@ import React, {
   useState 
 } from 'react';
 import { 
-    StyleSheet, 
     Text,  
     TextInput, 
     TouchableOpacity,
     View
 } from 'react-native';
 import { Picker } from '@react-native-community/picker';
+
+import { styles } from "./organizationEntry.style";
 
 const organizationEntry= (props) => {
   const [organizationnameName, setOrganizationName] = useState('');
@@ -71,50 +72,5 @@ const organizationEntry= (props) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0bda5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    fontWeight:"bold",
-    fontSize:20,
-    color:"#590208",
-    marginBottom:40
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#ebd8d9",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  forgot:{
-    color:"white",
-    fontSize:11
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#590208",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white"
-  }
-});
 
 export default organizationEntry;
