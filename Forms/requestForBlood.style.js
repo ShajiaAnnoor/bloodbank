40091d9435587f0a8ace534d95a0caab1bea6661
Shajia Annoor <shajiaannoor@gmail.com>
2021-10-15 18:45:37 +0600

    Refactor style to another file

---
+++ Forms/requestForBlood.style.js
@@ -0,0 +1,46 @@
+import { styles } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#f0bda5',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo:{
+      fontWeight:"bold",
+      fontSize:30,
+      color:"#590208",
+      marginBottom:40
+    },
+    inputView:{
+      width:"80%",
+      backgroundColor:"#ebd8d9",
+      borderRadius:25,
+      height:50,
+      marginBottom:20,
+      justifyContent:"center",
+      padding:20
+    },
+    inputText:{
+      height:50,
+      color:"white"
+    },
+    forgot:{
+      color:"white",
+      fontSize:11
+    },
+    loginBtn:{
+      width:"80%",
+      backgroundColor:"#590208",
+      borderRadius:25,
+      height:50,
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop:40,
+      marginBottom:10
+    },
+    loginText:{
+      color:"white"
+    }
+  });
