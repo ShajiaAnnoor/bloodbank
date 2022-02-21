6512cbe840bc6af5d6e3b5ef7c4406c157c767bc
ShajiaAnnoor <shajiaannoor@gmail.com>
2022-02-22 01:43:58 +0600

    Create reducer for adding blood request

--- redux/reducer/subscriptionlist.js
+++ redux/reducer/subscriptionlist.js
@@ -2,18 +2,44 @@ import {
   SET_LIST,
 } from "../constants/constants";
 
-const initialState = {
+/*const initialState = {
   subscriptionList: [],
   isLoading: false,
+}*/
+
+const initialState = {
+  bloodRequestList: [],
+  isLoading: false,
+
 }
 
-export default (state = initialState, action) => {
+/*export default (state = initialState, action) => {
   switch(action.type) {
     case SET_LIST:
       return {
         ...state,
         isLoading: true,
+
       };
+        
+      default:
+        return state;
+  }
+}*/
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_BLOOD_REQUEST:{
+      const bloodRequest= action.payload
+      return {
+        ...state,
+        bloodRequestList: {
+          ...state.bloodRequestList,
+          [bloodRequest.id]: bloodRequest,
+        
+        }
+      }
+    }
         
       default:
         return state;
