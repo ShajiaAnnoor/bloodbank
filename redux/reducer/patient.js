1fa03ce637fe29295463ebf116b1e69435892e4d
ShajiaAnnoor <shajiaannoor@gmail.com>
2022-02-25 21:48:39 +0600

    Create reducer for patient

---
+++ redux/reducer/patient.js
@@ -0,0 +1,78 @@
+import {
+    SET_LIST,
+  } from "../constants/constants";
+  
+  
+  const initialState = {
+    patientList: [],
+    isLoading: false,
+  
+  }
+  
+  
+  export default (state = initialState, action) => {
+    switch(action.type) {
+      case ADD_PATIENT:{
+        const patient= action.payload
+        return {
+          ...state,
+          patientList: {
+            ...state.patientList,
+            [patient.id]: patient,
+          
+          }
+        }
+      }
+
+      case FETCH_PATIENT_LIST:{
+          const newPatientList = {}
+          action.payload.forEach(patient => {
+            newPatientList[patient.id] = patient
+          })
+          return {
+            ...state,
+            patientList: newPatientList
+          }
+        
+      }
+
+      case UPDATE_PATIENT:{
+        const patient= action.payload
+        return {
+          ...state,
+          patientList: {
+            ...state.patientList,
+            [patient.id]: patient,
+          
+          }
+        }
+      }
+
+      case DELETE_DONOR:{
+        const newPatientList= { ...state.patientList }
+        delete newPatientList[action.payload]
+  
+        return {
+          ...state,
+          patientList: newPatientList
+        }
+      }
+
+      case FETCH_PATIENT:{
+        const patient= action.payload
+        return {
+          ...state,
+          patientList: {
+            ...state.patientList,
+            [patient.id]: patient,
+          
+          }
+        }
+      }
+  
+      
+          
+        default:
+          return state;
+    }
+  }
