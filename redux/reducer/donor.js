01077761238c567e6a3603e76089ae7a1abf37e1
ShajiaAnnoor <shajiaannoor@gmail.com>
2022-02-27 19:45:40 +0600

    Create reducers for showing and hiding loader

--- redux/reducer/donor.js
+++ redux/reducer/donor.js
@@ -69,6 +69,20 @@ import {
           }
         }
       }
+
+      case SHOW_LOADER: {
+        return {
+          ...state,
+          isLoading:true,
+        }
+      }
+
+      case HIDE_LOADER: {
+        return {
+          ...state,
+          isLoading:false,
+        }
+      }
   
       
           
