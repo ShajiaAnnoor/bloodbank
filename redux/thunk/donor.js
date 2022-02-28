45d75f6af36cbda3b6348e661fe9c76446db9cde
ShajiaAnnoor <shajiaannoor@gmail.com>
2022-02-28 15:48:34 +0600

    Modify actioncreator name to keep pace with the thunk function

--- redux/thunk/donor.js
+++ redux/thunk/donor.js
@@ -1,13 +1,13 @@
-import { addDonor } from '../action-creator/creator';
+import { addDonor, hideLoaderDonor, showLoaderDonor } from '../action-creator/creator';
 
 export async function addDonorThunk(dispatch, getState) {
 
     const isLoading = getState().donor.isLoading;
     
     if(isLoading == false){
-        dispatch(showLoader())
+        dispatch(showLoaderDonor())
         const response = await client.get('/fakeApi/todos')
-        dispatch(hideLoader())
+        dispatch(hideLoaderDonor())
         
 
         dispatch(addDonor(response))
