15af7fd1d1cae80bbe897acbc84a571225188801
ShajiaAnnoor <shajiaannoor@gmail.com>
2022-03-02 14:42:52 +0600

    create thunk for updating patient

--- redux/thunk/patient.js
+++ redux/thunk/patient.js
@@ -25,3 +25,21 @@ export async function fetchPatientListThunk(dispatch, getState) {
       dispatch(fetchPatientList(response))
 }
 }
+
+export async function updatePatientThunk(dispatch, getState) {
+
+  const isLoading = getState().patient.isLoading;
+  
+  if(isLoading == false){
+      dispatch(showLoaderPatient())
+      const response = await client.get('/fakeApi/todos')
+      dispatch(hideLoaderPatient())
+      if(response.ok==true){
+          dispatch(showSuccessMessage())
+      }
+      else{
+          dispatch(showFailureMessage())
+      }
+      dispatch(updatePatient(response))
+}
+}
