816e4851afd39d38e7172b8874b0effc1c192aa0
ShajiaAnnoor <shajiaannoor@gmail.com>
2022-03-09 14:16:20 +0600

    Add reducers to combine together

--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from "redux";
+import donor from "./donor";
+import organization from "./organization";
+import patient from "./patient";
 import subscriptionList from "./subscriptionlist";
 
 const rootReducer = combineReducers({
     "subscriptionList" : subscriptionList,
+    "bloodRequest" : bloodRequest,
+    "donor" : donor,
+    "organization" : organization,
+    "patient" : patient,
+
 })
 
 export default (state,action) => rootReducer(state,action);
