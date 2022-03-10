a4b60d64a92d1bbd6b83e3d80e5896e802b955b4
ShajiaAnnoor <shajiaannoor@gmail.com>
2022-03-10 15:35:09 +0600

    Create selector functions

--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -3,9 +3,9 @@ import donor from "./donor";
 import organization from "./organization";
 import patient from "./patient";
 import subscriptionList from "./subscriptionlist";
+import donor, * as fromDonor from "./donor";
 
 const rootReducer = combineReducers({
-    "subscriptionList" : subscriptionList,
     "bloodRequest" : bloodRequest,
     "donor" : donor,
     "organization" : organization,
@@ -14,3 +14,19 @@ const rootReducer = combineReducers({
 })
 
 export default (state,action) => rootReducer(state,action);
+
+export const getDonorList = state => fromDonor.getList(state.donor)
+export const getDonorStatus = state => fromDonor.getStatus(state.donor)
+export const getDonorIsLoading = state => fromDonor.getIsLoading(state.donor)
+export const getDonor = state => fromDonor.getDonor(state.donor)
+
+export const getPatientList = state => fromPatient.getList(state.patient)
+export const getPatientStatus = state => fromPatient.getStatus(state.patient)
+export const getPatientIsLoading = state => fromPatient.getIsLoading(state.patient)
+export const getPatient = state => fromPatient.getDonor(state.patient)
+
+export const getBloodRequestList = state => fromBloodRequest.getList(state.bloodRequest)
+export const getBloodRequestStatus = state => fromBloodRequest.getStatus(state.bloodRequest)
+export const getBloodRequestIsLoading = state => fromBloodRequest.getIsLoading(state.bloodRequest)
+export const getBloodRequest = state => fromBloodRequest.getBloodRequest(state.bloodRequest)
+
