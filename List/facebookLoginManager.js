baa6f9af42e7296d95c2ca18a9b94bbce34e98d9
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-08-26 05:09:39 +0600

    chore: neutralize facebook creds for now

--- List/facebookLoginManager.js
+++ List/facebookLoginManager.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { LoginManager } from "react-native-fbsdk-next";
-import { Settings } from 'react-native-fbsdk-next';
+//import { LoginManager } from "react-native-fbsdk-next";
 // ...
 
 // Attempt a login using the Facebook login dialog asking for default permissions.
 //Settings.initializeSDK();
+
 export const LogManager = function LM() {
-    
+  
+}
+/*
     LoginManager.logInWithPermissions(["public_profile"]).then(
         function(result) {
             if (result.isCancelled) {
@@ -24,3 +26,4 @@ export const LogManager = function LM() {
         }
     );
 }
+*/
