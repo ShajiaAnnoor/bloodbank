ee355d98e95c248667f4a694e630866fa2914583
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-08-25 12:00:22 +0600

    refactor: format code
    Keep the third libraries above the customized ones

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Provider } from "react-redux";
-import MyApp from './MyApp/myApp';
 import { PersistGate } from 'redux-persist/integration/react';
+
+import MyApp from './MyApp/myApp';
 import { store, persistor } from "./redux/store";
 
 export default function App() {
